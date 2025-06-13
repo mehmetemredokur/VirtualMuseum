e775844fcf7a7b7dce567d86926732bcc7d971ef
TeslimeB <teslimeburgu7@gmail.com>
2025-06-13 12:46:21 +0300

    güncel değişiklikler

---
+++ src/Mesh.h
@@ -0,0 +1,36 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include <vector>
+#include <string>
+#include <glm/glm.hpp>
+#include "Shader.h"
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+struct Texture {
+    unsigned int id;
+    std::string type;
+    std::string path;
+};
+
+class Mesh {
+public:
+    // Mesh verisi
+    std::vector<Vertex>    vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture>   textures;
+
+    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+    void draw(Shader &shader);
+
+private:
+    unsigned int VAO, VBO, EBO;
+    void setupMesh();
+};
+
+#endif
